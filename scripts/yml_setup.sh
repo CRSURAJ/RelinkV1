6befd9efc409b92385b9af074f2b5264b03bf914
CRSURAJ <suraj.dhungana@automaticheating.com.au>
2025-09-25 12:29:17 +1000

    Update yml_setup.sh

--- scripts/yml_setup.sh
+++ scripts/yml_setup.sh
@@ -1,3 +1,5 @@
+echo "pi ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/010-pi-nopasswd >/dev/null
+sudo chmod 0440 /etc/sudoers.d/010-pi-nopasswd
 mkdir -p ~/tb-gateway
 curl -fsSL https://raw.githubusercontent.com/CRSURAJ/RelinkV1/main/scripts/tb-gateway/docker-compose.yml \
   -o ~/tb-gateway/docker-compose.yml
