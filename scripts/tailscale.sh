a74ba2a8a002226059c0ef3ea2b344f4a39938ee
CRSURAJ <suraj.dhungana@automaticheating.com.au>
2025-09-25 10:39:55 +1000

    Update tailscale.sh

--- scripts/tailscale.sh
+++ scripts/tailscale.sh
@@ -1,6 +1,6 @@
 curl -fsSL https://tailscale.com/install.sh | sh
 sudo systemctl enable --now tailscaled
 sudo systemctl start tailscaled
-sudo tailscale up --auth-key=tskey-auth-kU1eF6C7ic11CNTRL-qVvoAWojrT9svN9EYcuJT958BpCJiNxUL
+sudo tailscale up --auth-key=
 sudo tailscale set --ssh
 sudo tailscale up --advertise-routes=192.168.0.0/24 --ssh
