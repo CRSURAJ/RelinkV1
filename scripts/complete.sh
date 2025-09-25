900b8e9ac037b53013d705c6df4e4793b53ff542
CRSURAJ <suraj.dhungana@automaticheating.com.au>
2025-09-25 14:18:58 +1000

    Update complete.sh

--- scripts/complete.sh
+++ scripts/complete.sh
@@ -40,7 +40,7 @@ echo "net.ipv4.ip_forward=1" | sudo tee -a /etc/sysctl.conf
 curl -fsSL https://tailscale.com/install.sh | sh
 sudo systemctl enable --now tailscaled
 sudo systemctl start tailscaled
-sudo tailscale up --auth-key=
+sudo tailscale up --auth-key=tskey-auth-kyVbwzeCPQ11CNTRL-eQVZunXhokCxKWHnZHLKmC2zsPufFeaT9
 sudo tailscale set --ssh
 sudo tailscale up --advertise-routes=192.168.0.0/24 --ssh
 
